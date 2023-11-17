b3aaf4f99e28cada625804588b9f616850b91429
YashwanthDumpa <127717110+YashwanthDumpa@users.noreply.github.com>
2023-11-17 10:21:16 +0530

    Update streamlit_app.py
    User input for Fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,12 @@ streamlit.dataframe(fruits_to_show)
 
 # API call 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+# Creating an input field for user choice
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 # Normalises the json response
